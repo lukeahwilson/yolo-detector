d93473ad969f7c29533a98d27087a91e849ecc10
Luke Wilson <luke.a.h.wilson@gmail.com>
2021-11-18 18:00:07 -0800

    Create Boundbox IOU Calculating Function

--- util.py
+++ util.py
@@ -176,20 +176,24 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
         if image_pred_.shape[0] == 0:
             continue
 
-        #Get the various classes detected in the image
+        # Get the various classes detected in the image, this looks like it might error if there are no detections above
         img_classes = unique(image_pred_[:,-1]) # -1 index holds the class index
 
-        #Time to perform the Non Max Suppresion algorithm
+        # Time to perform the Non Max Suppresion algorithm
+        # NOTE: I need to read this entire thing in details and really understand it better
+        # Iterate through the detected classes, one class at a time
         for cls in img_classes:
-            # NOTE: I need to read this entire thing in details and really understand it better
 
-            #get the detections with one particular class
+            # Get the detections with one particular class
             cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
+
+            # Not sure what the point of unsqueeze squeeze is.
             class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()
+
             image_pred_class = image_pred_[class_mask_ind].view(-1,7)
 
-            #sort the detections such that the entry with the maximum objectness
-            #confidence is at the top
+            # sort the detections such that the entry with the maximum objectness
+            # confidence is at the top
             conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
             image_pred_class = image_pred_class[conf_sort_index]
             idx = image_pred_class.size(0)   #Number of detections
@@ -212,6 +216,34 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
                 #Remove the non-zero entries
                 non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
                 image_pred_class = image_pred_class[non_zero_ind].view(-1,7)
+
+
+def bbox_iou(box1, box2):
+    """
+    Returns the IoU of two bounding boxes
+
+
+    """
+    #Get the coordinates of bounding boxes
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+
+    #get the corrdinates of the intersection rectangle
+    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+
+    #Intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+
+    #Union Area
+    b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area)
+
+    return iou
 
 
 # Function to return list of unique detected classes in the image from a list of detections
